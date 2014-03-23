9ace9615a3395b01bb4b2f7c7836bc3f743a6865
suisho <suisho000000@gmail.com>
2014-03-24 01:15:56 +0900

    split index

--- index.js
+++ index.js
@@ -1,64 +1,23 @@
-var csslib = require('css')
-var flatten = require('flatten')
-var migawari = require('migawari')
-var util = require('util')
-var fs = require('fs')
 var miyable = require('miyable')
-var async = require('async')
-var ruleReplace = require('./lib/rule_replace')
-var injectJs = require('./lib/inject_js')
-var pseudoReplace = require('./lib/pseudo_replace')
+var pd = require('pretty-data').pd
 var csstext = require('./lib/csstext')
+var makeshift = require('./lib/makeshift')
+var pseudoReplace = require('./lib/pseudo_replace')
+var ruleReplace = require('./lib/rule_replace')
+
 //var fixture = fs.readFileSync('./fixture/sample_gh.css', 'utf-8')
 //var fixture = fs.readFileSync('./fixture/sample1_sg.css', 'utf-8')
+var fs = require('fs')
 var fixture = fs.readFileSync('./fixture/a.css', 'utf-8')
 
 
-// On jsdom cannot compute pseudo.
-
-// Do Takigyo!
-function gyozui(cssSource){
-  
-  var ast = getAst(cssSource)
-  var selectors = getSelectors(ast)
-  var markups = createMarkup(selectors).join("\n")
-  var style = csslib.stringify(ast)
-  var js = [
-    fs.readFileSync("./tmp/b.js"),
-    injectJs(selectors)
-  ].join(";\n")
-  
-
-  var html = createHtml(markups, style, js)
-  console.log(html)
-  
-}
-
 function takigyo(cssSource){
-  var ast = getAst(cssSource)
-  var selectors = getSelectors(ast)
-
-  var markups = createMarkup(selectors).join("\n")
-  var style = csslib.stringify(ast)
-  
-  var html = createHtml(markups, style)
-  compute(html, selectors, function(err, result){
+  var ms = makeshift(cssSource)
+  var html = ms.html()
+  compute(html, ms.selectors, function(err, result){
     //console.log((result))
-    console.log(reparse(result))
+    console.log(toCss(result))
   })
-}
-
-
-function createHtml(markup, style, script){
-  return ["<html>"
-            , "<head>"
-              , "<style>" , style , "</style>"
-            , "</head>"
-            , "<body>"
-                , markup
-              , "<script>" , script , "</script>"
-            , "</body>"
-          , "</html>"].join("\n")
 }
 
 function compute(html, selectors, cb){
@@ -68,49 +27,16 @@ function compute(html, selectors, cb){
       var restore = pseudoReplace.restore(key)
       restored[restore] = result[key]
     })
-
     cb(err, restored)
   })
 }
 
-function reparse(result){
+function toCss(result){
   var output = Object.keys(result).map(function(selector){
     var text = csstext(result[selector])
     return selector + "{" + text + "}"
   }).join("")
-  return require('pretty-data').pd.css(output)
-}
-
-
-function getAst(cssSource){
-  var ast = csslib.parse(cssSource)
-  if(!ast.type == "stylesheet"){
-    return
-  }
-  ast = ruleReplace(ast, pseudoReplace.replace)
-  return ast
-}
-
-function getSelectors(ast){
-  
-  var rules = ast.stylesheet.rules.filter(function(rule){
-    return (rule.type === "rule")
-  })
-
-  var selectors = flatten(rules.map(function(rule){
-    return rule.selectors
-  }))
-
-  return selectors
-}
-
-function createMarkup(selectors){
-  var markups = selectors.map(function(selector){
-    return migawari(selector)
-  })
-
-  return markups
+  return pd.css(output)
 }
 
 takigyo(fixture)
-//gyozui(fixture)
