81f7372f52aac6484322ee119da6d5e8983e6eb8
suisho <suisho000000@gmail.com>
2014-03-28 02:32:19 +0900

    remove some files

--- engine/native.js
+++
@@ -1,7 +0,0 @@
-var computeStyles = require("../lib/compute_style.js")
-
-module.exports = function(html, selectors, cb){
-  computeStyles(window, selectors, function(err, result){
-    cb(err, result)
-  })
-}
