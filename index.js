a70b36221d9314c5ca64bac35011debe3e2dd607
suisho <suisho000000@gmail.com>
2014-03-28 02:28:52 +0900

    v2

---
+++ index.js
@@ -0,0 +1,39 @@
+var analyse = require("./lib/analyse")
+var pseudopseudo = require("pseudopseudo")
+var CSSselect = require('CSSselect')
+var makedom = require("htmlparser2").parseDOM;
+var async = require('async')
+var migawari = require('migawari')
+var util = require('util')
+var merge = require('defaults')
+var flatten = require('flatten')
+
+var extractStyles = function(analyzed, selector){
+  var html = migawari(selector)
+  var dom = makedom(html)
+  var styles = []
+  analyzed.sortedSelectors.forEach(function(s){
+    if(CSSselect.selectOne(s, dom) == null){
+      return
+    }
+    // override style
+    var styl = analyzed.styleDeclarations[selector]
+    styles.push(styl)
+
+  })
+  return flatten(styles)
+}
+
+var calcStyles = function(styles){
+  
+}
+
+module.exports = function(css){
+  var analyzed = analyse(css)
+  async.map(analyzed.selectors, function(selector, next){
+    var s = extractStyles(analyzed, selector)
+    next(null,{"selector" : selector, styles : s})
+  }, function(err, result){
+    console.log(result)
+  })
+}
---
+++ test/index.js
@@ -0,0 +1,10 @@
+var fs = require('fs')
+var takigyo = require('../index.js')
+//var fixture = fs.readFileSync('./fixture/sample_gh.css', 'utf-8')
+//var fixture = fs.readFileSync('./fixture/sample1_sg.css', 'utf-8')
+var css = fs.readFileSync('./fixture/a.css', 'utf-8')
+
+
+describe("", function(){
+  console.log(takigyo(css))
+})
