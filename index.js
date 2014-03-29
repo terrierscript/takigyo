7d361234a03e323ec025d68d4253c967b951c914
suisho <suisho000000@gmail.com>
2014-03-29 17:44:25 +0900

    use csstext

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var extractSelector = require('./lib/extract_selector')
 var styleRide = require('./lib/style_ride.js')
 var pd = require('pretty-data').pd
 var pseudopseudo = require('pseudopseudo')
+var csstext = require('csstext')
 
 var extractStyles = function(analyzed, selector){
   var extracted = extractSelector(analyzed.sortedSelectors, selector)
@@ -12,20 +13,9 @@ var extractStyles = function(analyzed, selector){
   })
 }
 
-var cssStringify = function(obj){
-  var cssqs = require('querystring')
-  // no escape
-  cssqs.escape = function(str){
-    return str
-  }
-  return cssqs.stringify(obj, ";", ":")
-}
 var toCss = function(selector, styles){
   var sel = pseudopseudo.restore(selector)
-  var style = cssStringify(styles)
-  if(style){
-    style += ";"
-  }
+  var style = csstext(styles)
   return sel + "{"+ style +"}"
 }
 
