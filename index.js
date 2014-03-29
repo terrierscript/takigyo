dbab2770336b573f4098102b242f564f584ebaaa
suisho <suisho000000@gmail.com>
2014-03-29 20:06:46 +0900

    split css convert

--- index.js
+++ index.js
@@ -6,28 +6,38 @@ var pd = require('pretty-data').pd
 var pseudopseudo = require('pseudopseudo')
 var csstext = require('csstext')
 
-var extractStyles = function(analyzed, selector){
-  var extracted = extractSelector(analyzed.sortedSelectors, selector)
-  return extracted.map(function(ex){
+var extractStyles = function(analyzed, selectors){
+  return selectors.map(function(ex){
     return analyzed.styles[ex]
   })
 }
 
 var toCss = function(selector, styles){
   var sel = pseudopseudo.restore(selector)
-  var style = csstext(styles)
+  var style = csstext.stringify(styles)
   return sel + "{"+ style +"}"
 }
 
 var takigyo = function(css, cb){
   var analyzed = analyse(css)
   async.map(analyzed.selectors, function(selector, next){
-    var styles = extractStyles(analyzed, selector)
-    var css = toCss(selector, styleRide(styles) )
-    next(null, css)
-  }, function(err, result){
-    var css = pd.css( result.join('') )
-    cb(err,css)
+    //var styles = extractStyles(analyzed, selector)
+    //var css = toCss(selector, styleRide(styles) )
+    var extracted = extractSelector(analyzed.sortedSelectors, selector)
+    var obj = {}
+    next(null, {
+      selector : selector,
+      override : extracted
+    })
+  }, function(err, hits){
+    var css = hits.map(function(item){
+      var selector = item["selector"]
+      var override = item["override"]
+      var styles = extractStyles(analyzed, override)
+      return toCss(selector, styleRide(styles) )
+    }).join("\n")
+    css = pd.css(css)
+    cb(err, css)
   })
 }
 
